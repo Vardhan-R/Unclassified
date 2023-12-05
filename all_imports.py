610e076468113b79a604b6703987fbf74dfc0661
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2023-12-05 17:24:30 +0530

    Create all_imports.py

---
+++ all_imports.py
@@ -0,0 +1,68 @@
+from built_modules import import_ann
+from built_modules import import_geneticalgorithm
+from built_modules import import_matrices
+from built_modules import import_number_system_converter
+from built_modules import import_vectors
+import collections
+from copy import deepcopy
+import csv
+import cv2
+import datetime
+import email
+from email.header import decode_header
+import imaplib
+import itertools
+import manim
+import math
+from matplotlib.animation import FuncAnimation
+import matplotlib.colors
+import matplotlib.patches
+import matplotlib.pyplot
+import multiprocessing
+import numpy
+import OpenGL
+import OpenGL.GL
+import OpenGL.GLU
+import os
+import pandas
+from perlin_noise import PerlinNoise
+import PIL
+import PIL.Image
+import pyaudio
+import pyfirmata
+import pygame
+import pynput
+from pynput.keyboard import Key
+from pynput.mouse import Button
+import pyperclip
+import pytesseract.pytesseract
+import pytz
+import random
+import requests
+import scipy
+from scipy.fft import fft
+from scipy.fft import fftfreq
+import seaborn
+import selenium
+import selenium.webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+import shelve
+import sklearn
+import smbus
+import smtplib
+import sounddevice
+import ssl
+import struct
+import sys
+import tensorflow
+from tensorflow.python.keras import datasets
+import tensorflow.python.keras.layers
+import tensorflow.python.keras.models
+import threading
+import time
+import wave
+import webbrowser
